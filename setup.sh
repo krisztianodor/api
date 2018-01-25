72f957b290c928f784bb038ce721201057c857b8
Jake <krazyjakee@gmail.com>
2018-01-25 12:26:52 +0100

    add wget

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+brew install wget
 wget https://github.com/gohugoio/hugo/releases/download/v0.18.1/hugo_0.18.1_macOS-64bit.zip
 unzip hugo_0.18.1_macOS-64bit.zip
 mv hugo_0.18.1_darwin_amd64/hugo_0.18.1_darwin_amd64 /usr/local/bin/hugo
