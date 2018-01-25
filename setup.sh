7aeb00ef86f52d0b227d5ea79ef91277b662dd5a
Jake <krazyjakee@gmail.com>
2018-01-25 12:25:21 +0100

    better setup script

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-brew install hugo
+wget https://github.com/gohugoio/hugo/releases/download/v0.18.1/hugo_0.18.1_macOS-64bit.zip
+unzip hugo_0.18.1_macOS-64bit.zip
+mv hugo_0.18.1_darwin_amd64/hugo_0.18.1_darwin_amd64 /usr/local/bin/hugo
+brew link hugo
+rm -f -r hugo_0.18.1_darwin_amd64
+rm hugo_0.18.1_macOS-64bit.zip
+hugo version
