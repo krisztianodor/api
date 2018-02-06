94ce43704534f9c2c95863bb40481d4e770f7d9c
Emil Åhsberg <lovet1991@hotmail.com>
2018-02-06 14:02:33 +0100

    Testing new bash script for changing documentation

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 hugo --destination "/tmp/igdb-api"
-if git checkout gh-pages; then rm -rf * && cp -rf /tmp/igdb-api/* .; fi
+if git checkout gh-pages; then rm -rf * && cp -rf /tmp/igdb-api/* . && git commit -m "Derp" && git push && git checkout master; fi
