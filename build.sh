d2f601a0def18bbdf410e8fc00d051a3baa98cb4
Jake Cattrall <krazyjakee@gmail.com>
2018-02-08 23:38:49 +0100

    build

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+rm -rf /tmp/igdb-api
 hugo --destination "/tmp/igdb-api"
 if git checkout gh-pages; then
     rm -rf *
     cp -rf /tmp/igdb-api/* .
+    git add -A
     git commit -m "Derp"
     git push
     git checkout master
