a1249b87a564c8b7406eada81c136cde5a718338
Jake Cattrall <krazyjakee@gmail.com>
2018-02-08 23:34:01 +0100

    multiline

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 hugo --destination "/tmp/igdb-api"
-if git checkout gh-pages; then rm -rf * && cp -rf /tmp/igdb-api/* . && git commit -m "Derp" && git push && git checkout master; fi
+if git checkout gh-pages; then
+    rm -rf *
+    cp -rf /tmp/igdb-api/* .
+    git commit -m "Derp"
+    git push
+    git checkout master
+fi
